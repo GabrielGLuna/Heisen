f2bf429b1a382624a7fd1746a0421954c8a4c5c9
GabrielGL <gabrielunagarcia@hotmail.com>
2025-07-12 10:54:07 -0600

    first commit

---
+++ components/Home/Header.tsx
@@ -0,0 +1,14 @@
+export default function HeaderHome(){
+    return(
+        <div className="w-full bg-[url(/images/cine.jpg)] lg:grid lg:grid-cols-2 lg:h-screen lg:bg-cover lg:w-full sm:bg-contain bg-no-repeat sm:not-visited:bg-center ">
+            <div className="justify-items-center content-center text-4xl">
+                <h1>Hola a Todos</h1>
+                <h2>Somos la mera polla con cebolla</h2>
+                <h3>Busca aquí tus pelis mai</h3>
+            </div>
+            <div>
+                <button>Saco</button>
+            </div>
+        </div>
+    )
+}
